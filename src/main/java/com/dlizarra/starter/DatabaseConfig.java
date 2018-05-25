292595257fdd1592d9610e09bc2f1d6e73faa817
Jiang Liu <liujiang682@gmail.com>
2018-05-25 14:07:36 +1000

    Modified README.md

--- src/main/java/com/dlizarra/starter/DatabaseConfig.java
+++ src/main/java/com/dlizarra/starter/DatabaseConfig.java
@@ -10,44 +10,44 @@ import org.springframework.core.env.Environment;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
+//@Configuration
 public class DatabaseConfig {
-
-	@Profile({ StarterProfiles.STANDALONE, StarterProfiles.TEST })
-	@PropertySource("classpath:application-default.properties") // Not loaded by naming convention
-	@Configuration
-	static class StandaloneDatabaseConfig {
-		@Bean
-		public DataSource dataSource(final Environment env) {
-			final HikariDataSource ds = new HikariDataSource();
-			ds.setJdbcUrl(env.getRequiredProperty("h2.jdbcurl"));
-			ds.setUsername(env.getRequiredProperty("h2.username"));
-			return ds;
-		}
-	}
-
-	@Profile(StarterProfiles.STAGING)
-	@Configuration
-	static class StagingDatabaseConfig {
-		@Bean
-		public DataSource dataSource(final Environment env) {
-			final HikariDataSource ds = new HikariDataSource();
-			ds.setJdbcUrl(env.getRequiredProperty("psql.jdbcurl"));
-			ds.setUsername(env.getRequiredProperty("psql.username"));
-			return ds;
-		}
-	}
-
-	@Profile(StarterProfiles.PRODUCTION)
-	@Configuration
-	static class ProuctionDatabaseConfig {
-		@Bean
-		public DataSource dataSource(final Environment env) {
-			final HikariDataSource ds = new HikariDataSource();
-			ds.setJdbcUrl(env.getRequiredProperty("psql.jdbcurl"));
-			ds.setUsername(env.getRequiredProperty("psql.username"));
-			return ds;
-		}
-	}
+//
+//	@Profile({ StarterProfiles.STANDALONE, StarterProfiles.TEST })
+//	@PropertySource("classpath:application-default.properties") // Not loaded by naming convention
+//	@Configuration
+//	static class StandaloneDatabaseConfig {
+//		@Bean
+//		public DataSource dataSource(final Environment env) {
+//			final HikariDataSource ds = new HikariDataSource();
+//			ds.setJdbcUrl(env.getRequiredProperty("h2.jdbcurl"));
+//			ds.setUsername(env.getRequiredProperty("h2.username"));
+//			return ds;
+//		}
+//	}
+//
+//	@Profile(StarterProfiles.STAGING)
+//	@Configuration
+//	static class StagingDatabaseConfig {
+//		@Bean
+//		public DataSource dataSource(final Environment env) {
+//			final HikariDataSource ds = new HikariDataSource();
+//			ds.setJdbcUrl(env.getRequiredProperty("psql.jdbcurl"));
+//			ds.setUsername(env.getRequiredProperty("psql.username"));
+//			return ds;
+//		}
+//	}
+//
+//	@Profile(StarterProfiles.PRODUCTION)
+//	@Configuration
+//	static class ProuctionDatabaseConfig {
+//		@Bean
+//		public DataSource dataSource(final Environment env) {
+//			final HikariDataSource ds = new HikariDataSource();
+//			ds.setJdbcUrl(env.getRequiredProperty("psql.jdbcurl"));
+//			ds.setUsername(env.getRequiredProperty("psql.username"));
+//			return ds;
+//		}
+//	}
 
 }
